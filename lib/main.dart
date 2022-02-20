32d88277aab5c5f06540a79abc653db2522c2c16
wallshock <87365865+wallshock@users.noreply.github.com>
2022-02-20 18:56:15 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
-        ),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
       ),
-    );
+    ),
+  );
+}
