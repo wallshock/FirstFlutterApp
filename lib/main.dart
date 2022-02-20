c4d005859d28887aa080aadd5b08c5b1fa4b98a4
wallshock <87365865+wallshock@users.noreply.github.com>
2022-02-20 20:42:17 +0100

    color change, added image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 68, 57, 57),
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Color Palette"),
+          ),
+          backgroundColor: Color.fromARGB(255, 36, 26, 26),
+        ),
+        body: Center(
+          child: Image.network(
+            'https://apkplz.net/storage/images/com/MustachioedPizza/SuperIdol/com.MustachioedPizza.SuperIdol_1.png',
+          ),
+        ),
       ),
     ),
   );
