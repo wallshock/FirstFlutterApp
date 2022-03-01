128ae9fb5fde36c7aa896008f6c2a18dd497ed35
Piotr Olszak <wallshoccfella@gmail.com>
2022-03-01 16:04:10 +0100

    app skeleton firebase connections auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 68, 57, 57),
-        appBar: AppBar(
-          title: const Center(
-            child: Text("Color Palette"),
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return const Scaffold(
+            body: Center(
+              child: Text("Error"),
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Root();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Scaffold(
+          body: Center(
+            child: Image.network(
+              'https://i.gifer.com/8CLc.gif',
+            ),
           ),
-          backgroundColor: Color.fromARGB(255, 36, 26, 26),
-        ),
-        body: Center(
-          child: Image.network(
-            'https://apkplz.net/storage/images/com/MustachioedPizza/SuperIdol/com.MustachioedPizza.SuperIdol_1.png',
-          ),
-        ),
-      ),
-    ),
-  );
+        );
+      },
+    );
+  }
+}
+
+class Root extends StatefulWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(stream: //auth stream);
+  }
 }
